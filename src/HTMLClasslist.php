df1890a0693e86c4a5282d40f444bb2544d349db
John Evans <john.evans9103@gmail.com>
2016-04-30 22:26:41 +0100

    Add 'add' method test cases

--- src/HTMLClasslist.php
+++ src/HTMLClasslist.php
@@ -5,7 +5,31 @@ class HTML_Classlist {
 	private $classes = array();
 
 	function __construct($classes = '') {
+		$this->add($classes);	
 		return $this;
+	}
+
+	private function sanitise_input($classes = '') {
+
+
+		return $classArray;
+	}
+
+	public function add($classes = '') {
+		$classes = $this->sanitise_input($classes);
+
+		return $this;
+	}
+
+	public function getOutput() {
+		if(count($this->classes) > 0)
+			return implode(' ', $this->classes);
+		else
+			return '';
+	}
+
+	public function output() {
+		echo $this->getOutput();
 	}
 
 }
--- tests/HTMLClasslistTest.php
+++ tests/HTMLClasslistTest.php
@@ -2,8 +2,85 @@
 
 class HTML_Classlist_Test extends PHPUnit_Framework_TestCase {
 	
+	public function classesToAddData() {
+		return array(
+			'blank string' => ['', ''],
+			'empty array' => [array(), ''],
+			'boolean true' => [true, ''],
+			'boolean false' => [false, ''],
+			'single string' => ['test', 'test'],
+			'multi string' => ['test something', 'test something'],
+			'string with duplicate' => ['test test', 'test'],
+			'multi string with duplicate' => ['test something test', 'test something'],
+			'string with excess whitespace' => ['  test   something ', 'test something'],
+			'string with tab' => ["test\tsomething", 'test something'],
+			'single item array' => [array('test'), 'test'],
+			'single item array with whitespace' => [array(' test  '), 'test'],
+			'single item array with duplicate' => [array('test', 'test'), 'test'],
+			'array with array item' => [array(array('test')), ''],
+			'array with boolean item' => [array(false), ''],
+			'multi item array' => [array('test', 'something'), 'test something'],
+			'multi item array with blank' => [array('test', 'something', ''), 'test something'],
+			'multi item array with duplicate' => [array('test', 'something', 'test'), 'test something']
+		);
+	}
+
+	public function invalidClassesToAddData() {
+		return array(
+			'number start' => ['2test'],
+			'star start' => ['*test'],
+			'dot start' => ['.test'],
+			'hash start' => ['#test'],
+		);
+	}
+
 	public function testCreation() {
 		$this->assertInstanceOf('HTML_Classlist', new HTML_Classlist);
+	}
+
+	public function testGetOutput() {
+		$cl = new HTML_Classlist('test');
+
+		$this->assertEquals('test', $cl->getOutput());
+
+		return $cl;
+	}
+
+	/**
+	 * @depends testGetOutput
+	 */
+	public function testOutput($cl) {
+		ob_start();
+
+		$cl->output();
+
+		$this->assertEquals('test', ob_get_clean());
+	}
+
+	/**
+	 * @dataProvider classesToAddData
+	 */
+	public function testAdd($input, $expectedOutput) {
+		$cl = new HTML_Classlist();
+		$cl->add($input);
+		$this->assertEquals($expectedOutput, $cl->getOutput());
+	}
+
+	/**
+	 * @dataProvider classesToAddData
+	 */
+	public function testContrsuctorAdd($input, $expectedOutput) {
+		$cl = new HTML_Classlist($input);
+		$this->assertEquals($expectedOutput, $cl->getOutput());
+	}
+
+	/**
+	 * TODO
+	 * @dataProvider invalidClassesToAddData
+	 * @expectedException PHPUnit_Framework_Error_Warning
+	 */
+	public function testInvalidAddTriggerWarning($input, $expectedOutput) {
+		$cl = new HTML_Classlist($input);
 	}
 
 }
